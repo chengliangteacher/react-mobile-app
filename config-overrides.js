d05057cd017637c834e2be112331630c69b004d1
chengliang <apple@AppledeMacBook-Pro.local>
2020-12-01 16:57:10 +0800

    引入less

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,40 @@
-const { override, fixBabelImports } = require('customize-cra');
-
+const { override, fixBabelImports } = require("customize-cra")
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: "ant-mobile",
-        style: "css"
-    })
+    fixBabelImports("import", {
+        libraryName: "antd-mobile",
+        style: true
+    }),
+    config => {
+        const oneOf_loc = config.module.rules.findIndex(n => n.oneOf);
+        config.module.rules[oneOf_loc].oneOf = [
+            {
+                test: /\.less$/,
+                use: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader'),
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            ident: 'postcss',
+                            javascriptEnabled: true,
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                            ],
+                        },
+                    },
+                    {
+                        loader: require.resolve('less-loader'),
+                        options: {
+                            lessOptions: {
+                                javascriptEnabled: true,
+                                
+                            }
+                        }
+                    },
+                ]
+            },
+            ...config.module.rules[oneOf_loc].oneOf
+        ]
+        return config
+    }
 )
